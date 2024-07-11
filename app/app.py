fc4f61e3d2b808b0dac936c2e9357f1f577179d6
iamdpastore <domenico.pastore93@gmail.com>
2024-07-11 18:59:45 +0200

    add cleanup

--- app/app.py
+++ app/app.py
@@ -36,5 +36,10 @@ def results():
     votes = {k.decode('utf-8'): int(v) for k, v in r.hgetall('votes').items()}
     return jsonify(votes)
 
+@app.route('/clear', methods=['POST'])
+def clear():
+    r.flushdb()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000) # Default port for Render Web Services
