76f1869560366794eff47998115b3c9900dd0ef7
iamdpastore <domenico.pastore93@gmail.com>
2024-07-12 09:17:20 +0200

    add map

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import redis
 import os
+import folium
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 app = Flask(__name__)
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
@@ -12,6 +15,14 @@ def get_client_ip():
     else:
         ip = request.remote_addr
     return ip
+
+def get_location(ip):
+    try:
+        location = geolocator.geocode(ip)
+        if location:
+            return (location.latitude, location.longitude)
+    except GeocoderTimedOut:
+        return None
 
 @app.route('/')
 def index():
@@ -29,6 +40,11 @@ def vote():
     
     r.hincrby('votes', candidate, 1)
     r.sadd('voted_ips', ip)
+
+    location = get_location(ip)
+    if location:
+        r.geoadd('locations', (location[1], location[0], ip))
+
     return redirect(url_for('index'))
 
 @app.route('/results')
@@ -40,6 +56,15 @@ def results():
 def clear():
     r.flushdb()
     return redirect(url_for('index'))
+
+@app.route('/map')
+def map_view():
+    m = folium.Map(location=[0, 0], zoom_start=2)
+    locations = r.georadius('locations', 0, 0, 10000000, unit='km', withcoord=True)
+    for loc in locations:
+        ip, (lon, lat) = loc[0].decode('utf-8'), loc[1]
+        folium.Marker(location=[lat, lon], popup=ip).add_to(m)
+    return m._repr_html_()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000) # Default port for Render Web Services
